ee8de55381267be1d8a814eff02bc633c90e284e
Uladzimir Karalchuk <korolchuk1986@gmail.com>
2019-03-27 12:04:09 +0100

    Remove duplicated directory

--- OneToMany/src/main/java/fr/ukaralchuk/library/onetomany/repo/AuthorRepository.java
+++
@@ -1,8 +0,0 @@
-package fr.ukaralchuk.library.onetomany.repo;
-
-import fr.ukaralchuk.library.onetomany.domain.Author;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AuthorRepository extends JpaRepository<Author, Long> {
-}
